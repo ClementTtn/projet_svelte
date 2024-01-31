9626975c1d5d57d95375b883de7327453bf84a42
Clément Tutin <clement.tutin.pro@gmail.com>
2024-01-31 20:36:50 +0100

    Commit final

--- sveltePokemon/src/lib/helpers.ts
+++
@@ -1,3 +0,0 @@
-export function rand(max: number, min = 0) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
--- sveltePokemon/src/routes/+layout.server.ts
+++
@@ -1,5 +0,0 @@
-import data from "$lib/data.json";
-
-export function load({depends}) {
-    return { pokemons: data };
-}
